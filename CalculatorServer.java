1ceb1f2ac5ee742abfac5bd7972704f02cc0d573
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2024-08-06 14:11:44 +0930

    adding comments into the server file

--- CalculatorServer.java
+++ CalculatorServer.java
@@ -4,18 +4,29 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class CalculatorServer
 {
+    /**
+     * The main method to start the Calculator RMI server
+     * Creates an instance of CalculatorImplementation 
+     * Exports it as a remote object and binds it to the RMI registry with the name "Calculator".
+     * @param args Argument inputs from command line or from a file 
+     */
     public static void main (String[] args)
     {
         try
         {
+            // Creating an instance of implementation class
             CalculatorImplementation calculator = new CalculatorImplementation();
-            Calculator stub = (Calculator) UnicastRemoteObject.exportObject(calculator, 0);
 
+            // Export as a remote object 
+            Calculator stub = (Calculator) UnicastRemoteObject.exportObject(calculator, 0);
+            
+            // Locate and bind the remote object's stub in the registry
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("Calculator", stub);
 
             System.err.println("Server ready");
         }
+        // Handle Exceptions 
         catch (Exception e)
         {
             System.err.println("Server exception: " + e.toString());
